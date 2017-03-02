9667f500ea8f54e69a931cc8f15618df9dcdbe76
irapani <uday.crazyuk@gmail.com>
2017-03-01 20:32:50 -0800

    Add files via upload

---
+++ angE11.js
@@ -0,0 +1,13 @@
+var app = angular.module('mainApp', ['ngRoute']);
+    app.config(function,($routeProvider) {
+               $routeProvider
+                .when('/', {
+                templateUrl: 'page.html'
+               })
+                .when('/helloUser', {
+                templateUrl: ' hello.html'
+            })
+                .otherwise({
+                redirectTo: '/'
+    });
+});
---
+++ angE7.js
@@ -0,0 +1,6 @@
+var application = angular.module('myLanguageApp', []);
+application.controller('languages', function ($scope) {
+    $scope.myFavLanguage = 'None';
+});
+    
+
