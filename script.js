39bce28d18723b1d330f7ce870a80217e995fb59
irapani <uday.crazyuk@gmail.com>
2017-05-18 22:38:58 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-var respose = require('./methods.js');
+	// closer ex
 
-respose.data.updateServer();
+/*var passed = 3;
+var addTo = function () {
+	var inner = 2;
+	return passed + inner;
+};
+
+	console.dir(addTo);*/
+
+	var passed = 4; 
+
+	var addTo = function (passed){
+		var add = function(inner){
+			return passed + inner
+
+		};
+			return add;
+	};
+		console.dir(addTo(3)); 
